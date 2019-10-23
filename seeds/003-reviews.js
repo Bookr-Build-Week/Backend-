dff1bb3080bf6b29e679b06434cc5908a93abffb
Mimi Hoang <miminhoang1@gmail.com>
2019-10-23 18:24:44 -0400

    made more seeds

--- seeds/003-reviews.js
+++ seeds/003-reviews.js
@@ -1,11 +1,11 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('users')
+  return knex('reviews')
     .then(function () {
       // Inserts seed entries
-      return knex('users').insert([
-        {username: 'asdfdghhj', password: '1445xdfgvf541' }
+      return knex('reviews').insert([
+       {contents: 'something', book_id: 1, user_id: 1}
       ]);
     });
 };
